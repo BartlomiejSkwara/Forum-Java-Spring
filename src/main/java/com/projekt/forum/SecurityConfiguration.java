68899c6916bc905503292dd69de0884e5e400ae3
Bartlomiej Skwara <bartlomiejskwara731@gmail.com>
2023-07-22 12:39:13 +0200

    Autoryzacja: -dodano konfiguracje dla SpringSecurity -logowanie i wylogowywanie działa -zmiana headera w zależności od zalogowanie -ochrona przed csrf

---
+++ src/main/java/com/projekt/forum/SecurityConfiguration.java
@@ -0,0 +1,90 @@
+package com.projekt.forum;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationProvider authenticationProvider) throws Exception {
+//        http    .csrf().disable()
+//                .authorizeHttpRequests().anyRequest().hasRole("USER")
+//                .and()
+//                .authenticationProvider(authenticationProvider)
+//                .addFilterBefore(
+//                        jwtAuthenticationFilter
+//                )
+//        ;
+
+            http.logout(logout->logout
+                            .logoutSuccessUrl("/")
+                            .permitAll()
+                    );
+//            http.logout(logout->{
+//                logout.logoutUrl("/logout");
+//
+//            });
+            http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {authorizationManagerRequestMatcherRegistry
+                    //.requestMatchers("/").authenticated()
+                    .anyRequest().permitAll();
+            });
+
+            http.formLogin(httpSecurityFormLoginConfigurer -> {
+                httpSecurityFormLoginConfigurer
+                        .loginPage("/login")
+
+                ;
+            });
+
+            http.httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(PasswordEncoder encoder){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        InMemoryUserDetailsManager  userDetailsService = new InMemoryUserDetailsManager();
+        userDetailsService.createUser(User.withUsername("Krisent").password(encoder.encode("123")).roles("USER").build());
+
+        provider.setUserDetailsService(userDetailsService);
+
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+//    @Bean()
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//
+//        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        UserDetails user = User.withUsername("bruh").password(encoder.encode("moment")).roles("USER").build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//
+//    }
+
+
+}
